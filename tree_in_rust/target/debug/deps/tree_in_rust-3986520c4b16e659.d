37a45f2a852be86384da1b3cd89c3d30720ca8c4
nishan8583 <nishanmaharjan17@gmail.com>
2025-02-21 17:20:15 -0800

    removing

--- tree_in_rust/target/debug/deps/tree_in_rust-3986520c4b16e659.d
+++
@@ -1,9 +0,0 @@
-/home/nishan/projects/leetcode/tree_in_rust/target/debug/deps/libtree_in_rust-3986520c4b16e659.rmeta: src/main.rs Cargo.toml
-
-/home/nishan/projects/leetcode/tree_in_rust/target/debug/deps/tree_in_rust-3986520c4b16e659.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
--- tree_in_rust/target/debug/tree_in_rust.d
+++
@@ -1,1 +0,0 @@
-/home/nishan/projects/leetcode/tree_in_rust/target/debug/tree_in_rust: /home/nishan/projects/leetcode/tree_in_rust/src/main.rs
